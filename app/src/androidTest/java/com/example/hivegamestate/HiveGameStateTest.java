c9429c2d4fd5563d42f484568f0fbc537ce4d5af
hilderbr21 <54870346+hilderbr21@users.noreply.github.com>
2019-10-31 19:27:42 -0700

    Created HiveGameStateTest Class

---
+++ app/src/androidTest/java/com/example/hivegamestate/HiveGameStateTest.java
@@ -0,0 +1,41 @@
+package com.example.hivegamestate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HiveGameStateTest {
+
+    @Test
+    public void toString1() {
+    }
+
+    @Test
+    public void placePiece() {
+
+    }
+
+    @Test
+    public void movePiece() {
+    }
+
+    @Test
+    public void undo() {
+    }
+
+    @Test
+    public void quit() {
+    }
+
+    @Test
+    public void zoom() {
+    }
+
+    @Test
+    public void getTurn() {
+    }
+
+    @Test
+    public void setTurn() {
+    }
+}
---
+++ app/src/main/java/com/example/hivegamestate/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.hivegamestate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText theText;
+
+    // Below citation by Samuel Nguyen
+    /**
+     * External Citation
+     * Date: October 10, 2019
+     * Problem: I needed to access an element at an index in the bugList ArrayList.
+     * Resource: https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
+     * Solution: I used the get() function.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        theText = (EditText) findViewById(R.id.EditText);
+
+        //initialize HiveGameState
+        HiveGameState hiveGameState = new HiveGameState();
+
+        //initialize the the Run Test button
+        Button runTestButton = findViewById(R.id.runTestButton);
+
+        runTestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //clears the text once clicked
+                theText.setText("");
+
+                //the first instance of the game created
+                HiveGameState firstInstance = new HiveGameState();
+
+                //second instance created by deep copy from first instance
+                HiveGameState secondInstance = new HiveGameState(firstInstance);
+
+                //call all methods of firstInstance
+                //each method also requires a String to be appended to the editText object
+                firstInstance.placePiece(0, firstInstance.bugList.get(0), 5, 5);
+                theText.append("Player 0 moved " + firstInstance.bugList.get(0)
+                        + " to position [5][5]\n");
+                firstInstance.movePiece(0, firstInstance.bugList.get(0), 5, 5, 6, 6);
+                theText.append("Player 0 moved " + firstInstance.bugList.get(0) +
+                        " to position [6][6] from [5][5]\n");
+                firstInstance.undo(0);
+                theText.append("Moves undone by player 0.\n");
+                firstInstance.zoom(1);
+                theText.append("Game was zoomed in by player 1.\n");
+                int turn = firstInstance.getTurn();
+                theText.append("Got the turn, result is " + turn +".\n");
+                firstInstance.setTurn(0);
+                theText.append("Set the turn to player 0's turn.\n");
+                firstInstance.quit(1);
+                theText.append("Game quit by player 1.\n");
+
+
+                //the third instance of game created
+                HiveGameState thirdInstance = new HiveGameState();
+
+                //fourth instance deep copy made from the third instance
+                HiveGameState fourthInstance = new HiveGameState(thirdInstance);
+
+                //append second and fourth state strings to theText, then check if both are equal
+                theText.append(secondInstance.toString());
+                theText.append(fourthInstance.toString());
+                check(secondInstance, fourthInstance);
+            }
+        }
+        );
+
+    }
+
+    /**
+     * Checks to see whether two HiveGameStates have the same
+     * result from the toString() method
+     *
+     * arguments: HiveGameState arguments
+     * @return true if successful, false otherwise
+     */
+    public Boolean check(HiveGameState stateOne, HiveGameState stateTwo) {
+        if(stateOne.toString().equals(stateTwo.toString())) {
+            return true;
+        }
+        return false;
+    }
+}
